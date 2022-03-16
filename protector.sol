92802c451210a71b476591c6252ca85bf256e24e
Vito <farinavito@gmail.com>
2022-03-16 15:51:53 +0100

    added a title and an author

--- protector.sol
+++ protector.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
+/// @title A way to reduce the risk in smart contracts when one address is compromised.
+/// @author Farina Vito
+
 contract Protector {
 
     /// @notice Storing the owner's address
