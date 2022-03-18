c55736ee3e478c232800b41ca96ab17226d21419
Vito <farinavito@gmail.com>
2022-03-18 08:25:32 +0100

    added comments

--- protector.sol
+++ protector.sol
@@ -12,9 +12,11 @@ contract AddressProtector {
         address protectorAddress;    
     }
 
+    //maybe public
     /// @notice Adding votes for candidates by protectors
     mapping(address => mapping(address => bool)) internal alreadyVoted;
 
+    //maybe public
     /// @notice A unique identifier of the protector
     mapping (uint256 => protectorStruct) internal protectors;
 
