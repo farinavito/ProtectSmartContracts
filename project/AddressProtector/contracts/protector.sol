6d81f724dcb1c4417de7c8e51dee68b15e8f45fd
Vito <farinavito@gmail.com>
2022-03-18 17:27:02 +0100

    first contract modified

--- project/AddressProtector/contracts/protector.sol
+++ project/AddressProtector/contracts/protector.sol
@@ -17,9 +17,6 @@ contract AddressProtector {
 
     /// @notice A unique identifier of the protector
     mapping (uint256 => protectorStruct) public protectors;
-
-    /// @notice Candidate for protectorWaitingToBeOwner
-    mapping (address => uint256) internal candidates;
         
     /// @notice Storing the owner's address
     address internal protectorOwner;
@@ -29,6 +26,7 @@ contract AddressProtector {
 
     ///@notice Storing all protectors
     address[] internal allprotectorsaddresses;
+
 
     constructor (
         address _protectorOwner,
@@ -54,64 +52,6 @@ contract AddressProtector {
             newProtector.protectorId = i;
             newProtector.protectorAddress = allprotectorsaddresses[i - 1];
         }
-    }
-
-    /// @notice Only the protectorOwner can access
-    modifier onlyprotectorOwner(){
-        require(msg.sender == protectorOwner, "You are not the owner");
-        _;
-    }
-
-    /// @notice Only the whitelisted address can access
-    modifier onlyWhitelisted() {
-        require(whitelist[msg.sender], "You aren't whitelisted");
-        _;
-    }
-
-    /// @notice Whitelisted accounts that can access withdrawal_amount_owner
-    mapping(address => bool) public whitelist;
-
-    /// @notice Adding address to the whitelist
-    //check if there already exists a whitelist address
-    function addToWhitelist(address _address) external onlyprotectorOwner {
-        whitelist[_address] = true;
-    }
-    
-    /// @notice Removing address from the whitelist
-    //check if there already exists a whitelist address
-    function removedFromWhitelist(address _address) external onlyprotectorOwner {
-        whitelist[_address] = false;
-    }
-
-    /// @notice Changing the owner and the waitingToBeOwner
-    function changeOwner(address _nextInline) external {
-        require(protectorWaitingToBeOwner == msg.sender, "You don't have permissions");
-        require(protectorWaitingToBeOwner != _nextInline, "protectorWaitingToBeOwner can't be the same");
-        require(candidates[_nextInline] == 5, "Not all protectors agree with this address");
-        protectorOwner = protectorWaitingToBeOwner;
-        protectorWaitingToBeOwner = _nextInline;
-    }
-
-    /// @notice Adding candidates by protectors
-    function addCandidate(address _nextInLine, uint256 _id) external {
-        require(protectors[_id].protectorAddress == msg.sender, "You aren't a protector");
-        candidates[_nextInLine] = 0;
-    }
-
-    /// @notice Voting for candidates by protectors
-    function voteCandidate(address _nextInLine, uint256 _id) external {
-        require(protectors[_id].protectorAddress == msg.sender, "You aren't a protector");
-        require(alreadyVoted[msg.sender][_nextInLine] == false, "You have entered your vote");
-        alreadyVoted[msg.sender][_nextInLine] = true;
-        candidates[_nextInLine] += 1;
-    }
-
-    /// @notice remove vote by the protector from previously voted protectorWaitingToBeOwner
-    function removeVote(address _nextInLine, uint256 _id) external {
-        require(protectors[_id].protectorAddress == msg.sender, "You aren't a protector");
-        require(alreadyVoted[msg.sender][_nextInLine] == true, "You haven't voted for this address");
-        alreadyVoted[msg.sender][_nextInLine] = false;
-        candidates[_nextInLine] -= 1;
     }
     
 }
