be669f744b8bc218323acdf45552c7fe346ab1d3
Vito <farinavito@gmail.com>
2022-03-18 09:17:25 +0100

    added fixtures

--- project/tests/test_protectors.py
+++ project/tests/test_protectors.py
@@ -5,3 +5,16 @@ from brownie import *
 from brownie import accounts
 from brownie.network import rpc
 from brownie.network.state import Chain
+
+#addresses
+protectorOwnerAddres = 1
+protectorWaitingToBeOwnerAddress = 2
+addressProtector1 = 3
+addressProtector2 = 4
+addressProtector3 = 5
+addressProtector4 = 6
+addressProtector5 = 7
+
+@pytest.fixture()
+def deploy(AddressProtector, module_isolation):
+    return AddressProtector.deploy(accounts[1], accounts[2], accounts[3], accounts[4], accounts[5], accounts[6], accounts[7], {'from': accounts[0]})
