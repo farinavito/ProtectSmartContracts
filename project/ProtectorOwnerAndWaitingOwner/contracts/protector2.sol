62a740ae5f115fa68e41a45cb0b0e82111230971
Vito <farinavito@gmail.com>
2022-03-19 19:51:11 +0100

    changed candidates to candidatesVotes

--- project/ProtectorOwnerAndWaitingOwner/contracts/protector2.sol
+++ project/ProtectorOwnerAndWaitingOwner/contracts/protector2.sol
@@ -4,7 +4,8 @@ pragma solidity 0.8.11;
 /// @title A way to reduce the risk in smart contracts when one address is compromised.
 /// @author Farina Vito
 
-import "https://github.com/farinavito/ProtectSmartContracts/blob/main/project/AddressProtector/contracts/protector.sol";
+//import "https://github.com/farinavito/ProtectSmartContracts/blob/main/project/AddressProtector/contracts/protector.sol";
+import "../../protector.sol";
 
 contract ProtectorOwnerWaitingOwner is AddressProtector() {
 
@@ -21,7 +22,7 @@ contract ProtectorOwnerWaitingOwner is AddressProtector() {
     function changeOwner(address _nextInline) external {
         require(protectorWaitingToBeOwner == msg.sender, "You don't have permissions");
         require(protectorWaitingToBeOwner != _nextInline, "protectorWaitingToBeOwner can't be the same");
-        require(candidates[_nextInline] == 5, "Not all protectors agree with this address");
+        require(candidatesVotes[_nextInline] == 5, "Not all protectors agree with this address");
         protectorOwner = protectorWaitingToBeOwner;
         protectorWaitingToBeOwner = _nextInline;
     }
--- project/ProtectorOwnerAndWaitingOwner/tests/test_protectors2.py
+++ project/ProtectorOwnerAndWaitingOwner/tests/test_protectors2.py
@@ -16,3 +16,19 @@ addressProtector5 = 7
 @pytest.fixture()
 def deploy(AddressProtector, module_isolation):
     return AddressProtector.deploy(accounts[1], accounts[2], accounts[3], accounts[4], accounts[5], accounts[6], accounts[7], {'from': accounts[0]})
+
+
+
+'''TESTING CHANGEOWNER'''
+
+
+@pytest.mark.aaa
+def test_changeOwner_1st_require(deploy):
+    '''checking if the user has permissions to change the owner'''
+    assert deploy.changeOwner(accounts[9], {'from': accounts[1]}) == "ok"
+    '''
+    try:
+        deploy.changeOwner(accounts[9], {'from': accounts[1]})        
+    except Exception as e:
+        assert e.message[50:] == "The agreement's deadline has ended"
+    '''
