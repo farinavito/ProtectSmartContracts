153f481b8e90c04728a8f15b6e1fcc5a4b0995f8
Vito <farinavito@gmail.com>
2022-03-22 16:34:48 +0100

    created test_addToWhitelist_onlyprotectorOwner

--- project/ProtectorWhitelisted/contracts/test_protectors3.py
+++ project/ProtectorWhitelisted/contracts/test_protectors3.py
@@ -15,3 +15,18 @@ addressProtector5 = 7
 @pytest.fixture()
 def deploy(AddressProtector, module_isolation):
     return AddressProtector.deploy(accounts[protectorOwnerAddress], accounts[protectorWaitingToBeOwnerAddress], accounts[addressProtector1], accounts[addressProtector2], accounts[addressProtector3], accounts[addressProtector4], accounts[addressProtector5], {'from': accounts[0]})
+
+
+
+'''TESTING ADDTOWHITELIST'''
+
+@pytest.mark.parametrize("not_owner",  [addressProtector1, addressProtector2, addressProtector3, addressProtector4, addressProtector5])
+def test_addToWhitelist_onlyprotectorOwner(deploy, not_owner):
+    '''checking if only the protectorOwner can access this function'''
+    assert deploy.addToWhitelist(accounts[8], {'from': accounts[not_owner]})
+    '''
+    with brownie.reverts("You are not the owner"):
+        deploy.addToWhitelist(accounts[8], {'from': accounts[not_owner]})
+    '''
+    
+    
