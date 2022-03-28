37d091d4714c55933ee16c387cc679afe1dae847
Vito <farinavito@gmail.com>
2022-03-28 15:05:54 +0200

    added checkWhichProtector && its implementation

--- project/ProtectorOwnerAndWaitingOwner/contracts/protector2.sol
+++ project/ProtectorOwnerAndWaitingOwner/contracts/protector2.sol
@@ -14,26 +14,40 @@ contract ProtectorOwnerWaitingOwner is AddressProtector() {
     /// @notice Storing the created candidates
     mapping (address => bool) public existingCandidates;
 
+    /// @notice Checking if the input address is the protector
+    function checkWhichProtector(address _address) internal view returns(uint256 _i){
+        for (uint256 i = 0; i < 5; i++){
+            if (allprotectorsaddresses[i] == _address){
+                return i;
+            } else if (i != 4){
+                continue;
+            } else {
+                revert("You don't have permissions");
+            }
+        }
+    }
+
     /// @notice Changing the owner and the waitingToBeOwner
     function changeOwner(address _nextInline) external {
         require(protectorWaitingToBeOwner == msg.sender, "You don't have permissions");
         require(protectorWaitingToBeOwner != _nextInline, "protectorWaitingToBeOwner can't be the same");
         require(candidatesVotes[_nextInline] == 5, "Not all protectors agree with this address");
+        //reinitializing to 0
         protectorOwner = protectorWaitingToBeOwner;
         protectorWaitingToBeOwner = _nextInline;
     }
     
     /// @notice Voting for candidates by protectors
-    function voteCandidate(address _nextInLine, uint256 _id) external {
-        require(protectors[_id].protectorAddress == msg.sender, "The id entered isn't equal to protector's id");
+    function voteCandidate(address _nextInLine) external {
+        checkWhichProtector(msg.sender);
         require(alreadyVoted[msg.sender][_nextInLine] == false, "You have entered your vote");
         alreadyVoted[msg.sender][_nextInLine] = true;
         candidatesVotes[_nextInLine] += 1;
     }
 
     /// @notice remove vote by the protector from previously voted protectorWaitingToBeOwner
-    function removeVote(address _nextInLine, uint256 _id) external {
-        require(protectors[_id].protectorAddress == msg.sender, "You aren't a protector");
+    function removeVote(address _nextInLine) external {
+        checkWhichProtector(msg.sender);
         require(alreadyVoted[msg.sender][_nextInLine] == true, "You haven't voted for this address");
         alreadyVoted[msg.sender][_nextInLine] = false;
         candidatesVotes[_nextInLine] -= 1;
