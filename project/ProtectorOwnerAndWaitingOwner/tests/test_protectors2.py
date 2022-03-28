961c7b419421e9b63677d5ac270536060e82c969
Vito <farinavito@gmail.com>
2022-03-28 21:36:17 +0200

    removed the unused fixture

--- project/ProtectorOwnerAndWaitingOwner/tests/test_protectors2.py
+++ project/ProtectorOwnerAndWaitingOwner/tests/test_protectors2.py
@@ -12,11 +12,6 @@ addressProtector2 = 4
 addressProtector3 = 5
 addressProtector4 = 6
 addressProtector5 = 7
-'''
-@pytest.fixture()
-def deploy(AddressProtector, module_isolation):
-    return AddressProtector.deploy(accounts[protectorOwnerAddress], accounts[protectorWaitingToBeOwnerAddress], accounts[addressProtector1], accounts[addressProtector2], accounts[addressProtector3], accounts[addressProtector4], accounts[addressProtector5], {'from': accounts[0]})
-'''
 
 
 @pytest.fixture()
