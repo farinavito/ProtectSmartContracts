10cf7793e12ca4ae758651c054bf4f23ca679c37
Vito <farinavito@gmail.com>
2022-04-04 18:47:32 +0200

    added protector2's and protector3's code to protector

--- project/AddressProtector/contracts/protector.sol
+++ project/AddressProtector/contracts/protector.sol
@@ -8,6 +8,7 @@ pragma solidity 0.8.11;
 // 1. allprotectorsaddresses -> internal
 // 2. for loop -> i something smaller than uint256 -> constructor, checkWhichProtector, returnProtector, changeOwner,
 // 3. checkWhichProtector -> returns (uint256 _i), _i could be something smaller
+// 4. changeOwner -> maybe only 3 out of 5?
 
 contract AddressProtector {
 
@@ -16,6 +17,9 @@ contract AddressProtector {
     
     /// @notice Candidate for protectorWaitingToBeOwner
     mapping (address => uint256) public candidatesVotes;
+
+    /// @notice Whitelisted accounts that can access withdrawal_amount_owner
+    mapping(address => bool) public whitelist;
         
     /// @notice Storing the owner's address
     address public smartcontractOwner;
@@ -53,6 +57,78 @@ contract AddressProtector {
             candidatesVotes[protectorWaitingToBeOwner] += 1;
             alreadyVoted[allprotectorsaddresses[i - 1]][protectorWaitingToBeOwner] = true;
         }
+    }
+
+    /// @notice Checking if the input address is the protector
+    function checkWhichProtector(address _address) internal view returns(uint256 _i){
+        for (uint256 i = 0; i < 5; i++){
+            if (allprotectorsaddresses[i] == _address){
+                return i;
+            } else if (i != 4){
+                continue;
+            } else {
+                revert("You don't have permissions");
+            }
+        }
+    }
+
+    /// @notice Returning all addresses of protectors
+    function returnProtectors() external {
+        for (uint256 i = 0; i < 5; i++){
+            emit showAllProtectors(allprotectorsaddresses[i]);
+        }
+    }
+
+    /// @notice Changing the owner and the waitingToBeOwner
+    function changeOwner(address _nextInline) external {
+        require(protectorWaitingToBeOwner == msg.sender, "You don't have permissions");
+        require(candidatesVotes[_nextInline] == 5, "Not all protectors agree with this address");
+        //reinitializing to 0
+        candidatesVotes[smartcontractOwner] = 0;
+        for (uint256 i = 0; i < 5; i++){
+            alreadyVoted[allprotectorsaddresses[i]][smartcontractOwner] = false;
+        }
+
+        smartcontractOwner = protectorWaitingToBeOwner;
+        protectorWaitingToBeOwner = _nextInline;
+    }
+    
+    /// @notice Voting for candidates by protectors
+    function voteCandidate(address _nextInLine) external {
+        checkWhichProtector(msg.sender);
+        require(alreadyVoted[msg.sender][_nextInLine] == false, "You have entered your vote");
+        alreadyVoted[msg.sender][_nextInLine] = true;
+        candidatesVotes[_nextInLine] += 1;
+    }
+
+    /// @notice remove vote by the protector from previously voted protectorWaitingToBeOwner
+    function removeVote(address _nextInLine) external {
+        checkWhichProtector(msg.sender);
+        require(alreadyVoted[msg.sender][_nextInLine] == true, "You haven't voted for this address");
+        alreadyVoted[msg.sender][_nextInLine] = false;
+        candidatesVotes[_nextInLine] -= 1;
+    }
+
+    /// @notice Only the whitelisted address can access
+    modifier onlyWhitelisted() {
+        require(whitelist[msg.sender], "You aren't whitelisted");
+        _;
+    }
+
+    /// @notice Only the protectorOwner can access
+    modifier onlyprotectorOwner(){
+        require(msg.sender == smartcontractOwner, "You are not the owner");
+        _;
+    }
+
+    /// @notice Adding address to the whitelist
+    function addToWhitelist(address _address) external onlyprotectorOwner {
+        whitelist[_address] = true;
+    }
+    
+    /// @notice Removing address from the whitelist
+    function removedFromWhitelist(address _address) external onlyprotectorOwner {
+        whitelist[_address] = false;
     }
     
 }
